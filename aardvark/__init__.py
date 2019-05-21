27b026289e83a8162aed01b3f0cf71f643e8f609
Mihai Baciu <mbaciu@gopro.com>
2019-05-21 15:31:39 +0300

    DSP-1735 don't include config in image; instead, generate a config from template at runtime, using env and values from aws secret.

--- aardvark/__init__.py
+++ aardvark/__init__.py
@@ -1,12 +1,17 @@
+import boto3
+import json
+import os
 import os.path
 import logging
 from logging import DEBUG, Formatter, StreamHandler
 from logging.config import dictConfig
 import sys
 
+from jinja2 import Environment, PackageLoader, select_autoescape
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flasgger import Swagger
+
 
 db = SQLAlchemy()
 
@@ -25,8 +30,28 @@ def create_app():
 
     path = _find_config()
     if not path:
-        print('No config')
-        app.config.from_pyfile('_config.py')
+        print('No config. Trying to generate one from a template.')
+        try:
+            env = Environment(loader=PackageLoader('aardvark', 'templates'), autoescape=select_autoescape(['html', 'xml']))
+            config_template = env.get_template('config.py.j2')
+            config_json = json.loads(os.environ.get('config_json', {}))
+            if not set(['role_name', 'region', 'secret_id']) <= set(config_json.keys()) or \
+                    len([v for v in config_json.itervalues() if not v]) > 0:
+                raise Exception('At least one of config variables {k} missing or empty.'.format(k=', '.join(config_json.keys())))
+            session = boto3.session.Session(region_name=config_json['region'])
+            client = session.client('secretsmanager')
+            secret = json.loads(client.get_secret_value(SecretId=config_json['secret_id']))['SecretString']
+            with open('/etc/aardvark/config.py') as fd:
+                print >>fd, config_template.render(role_name=config_json['role_name'],
+                                                   region=config_json['region'],
+                                                   db_username=secret['username'],
+                                                   db_password=secret['password'],
+                                                   db_endpoint=secret['host'],
+                                                   db_name=secret['dbname'])
+        except Exception:
+            print('Failed generating config from template. Falling back to hardcoded values.')
+            # no template found; catchall
+            app.config.from_pyfile('_config.py')
     else:
         app.config.from_pyfile(path)
 
