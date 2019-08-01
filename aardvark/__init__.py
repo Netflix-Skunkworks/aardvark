2d66b5a55216dfd4c622a2a3a542d27eb3041937
Mihai Baciu <mbaciu@gopro.com>
2019-08-01 18:31:12 +0300

    upstream/master sync

--- aardvark/__init__.py
+++ aardvark/__init__.py
@@ -1,17 +1,12 @@
-import boto3
-import json
-import os
 import os.path
 import logging
 from logging import DEBUG, Formatter, StreamHandler
 from logging.config import dictConfig
 import sys
 
-from jinja2 import Environment, PackageLoader, select_autoescape
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flasgger import Swagger
-
 
 db = SQLAlchemy()
 
@@ -30,36 +25,8 @@ def create_app():
 
     path = _find_config()
     if not path:
-        print('No config. Trying to generate one from a template.')
-        try:
-            config_dir = '/etc/aardvark'
-            config_file = 'config.py'
-            config_fullpath = os.path.join(config_dir, config_file)
-            env = Environment(loader=PackageLoader('aardvark', 'templates'), autoescape=select_autoescape(['html', 'xml']))
-            config_template = env.get_template('config.py')
-            config_json = json.loads(os.environ.get('config_json', {}))
-            if not set(['role_name', 'region', 'secret_id']) <= set(config_json.keys()) or \
-                    len([v for v in config_json.itervalues() if not v]) > 0:
-                raise Exception('At least one of config variables {k} missing or empty.'.format(k=', '.join(config_json.keys())))
-            session = boto3.session.Session(region_name=config_json['region'])
-            client = session.client('secretsmanager')
-            secret_response = client.get_secret_value(SecretId=config_json['secret_id'])
-            secret = json.loads(secret_response['SecretString'])
-            if not os.path.isdir(config_dir):
-                os.mkdir(config_dir)
-            print('Generating config file')
-            with open(config_fullpath, 'w') as fd:
-                print >>fd, config_template.render(role_name=config_json['role_name'],
-                                                   aws_region=config_json['region'],
-                                                   db_username=secret['username'],
-                                                   db_password=secret['password'],
-                                                   db_endpoint=secret['host'],
-                                                   db_name=secret['dbname'])
-            app.config.from_pyfile(config_fullpath)
-        except Exception as e:
-            print('Failed generating config from template. Falling back to hardcoded values: {e}'.format(e=repr(e)))
-            # no template found; catchall
-            app.config.from_pyfile('_config.py')
+        print('No config')
+        app.config.from_pyfile('_config.py')
     else:
         app.config.from_pyfile(path)
 
