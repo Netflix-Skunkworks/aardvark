790f2812a297d6e5bcf9b312dfb61becb22e0feb
Doppins <contact@doppins.com>
2019-08-02 15:14:39 +0000

    Upgrade dependency flasgger to ==0.9.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ install_requires = [
     'Flask==0.12.1',
     'Flask-RESTful==0.3.5',
     'Flask-Script==2.0.5',
-    'flasgger==0.9.2',
+    'flasgger==0.9.3',
     'gunicorn==19.7.1',
     'psycopg2==2.7.1',
     'pytz==2017.2',
