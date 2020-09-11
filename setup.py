8563305057b6f1030e75201abbd74fe59367d6a0
Patrick Sanders <psanders@netflix.com>
2020-09-11 11:52:45 -0700

    use setupmeta to clean up setup and metadata

--- setup.py
+++ setup.py
@@ -1,59 +1,16 @@
 """
-Aardvark
-=====
-Multi-Account AWS IAM Access Advisor API
-:copyright: (c) 2020 by Netflix
-:license: Apache, see LICENSE for more details.
+name: aardvark
+description: Multi-Account AWS IAM Access Advisor API
+author: Patrick Kelley, Travis McPeak, Patrick Sanders
+maintainer: Patrick Sanders
+contact: aardvark-maintainers@netflix.com
 """
-from __future__ import absolute_import
-
-import os.path
-import sys
-
-from setuptools import find_packages, setup
-
-ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
-
-# When executing the setup.py, we need to be able to import ourselves, this
-# means that we need to add the src/ directory to the sys.path.
-sys.path.insert(0, ROOT)
-
-about = {}
-with open(os.path.join(ROOT, "aardvark", "__about__.py")) as f:
-    exec(f.read(), about)
-
-
-install_requires = [
-    "SQLAlchemy",
-    "Flask",
-    "blinker",
-    "cloudaux",
-    "confuse",
-    "bunch",
-    "flasgger",
-]
-
-tests_require = []
-
-docs_require = []
-
-dev_requires = []
-
+from setuptools import setup
 
 setup(
-    name=about["__title__"],
-    version=about["__version__"],
-    author=about["__author__"],
-    author_email=about["__email__"],
-    url=about["__uri__"],
-    description=about["__summary__"],
+    name="aardvark",
     python_requires="~=3.8",
-    long_description=open(os.path.join(ROOT, "README.md")).read(),
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    extras_require={"tests": tests_require, "docs": docs_require, "dev": dev_requires},
+    versioning="devcommit",
+    setup_requires="setupmeta",
     entry_points={"console_scripts": ["aardvark = aardvark.manage:main"]},
 )
