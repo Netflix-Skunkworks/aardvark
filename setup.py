9974a789fa01b919ce4bac45f2b736bb4b863568
Patrick Sanders <psanders@netflix.com>
2024-09-13 14:31:07 -0700

    make setup less hacky

--- setup.py
+++ setup.py
@@ -7,22 +7,14 @@ Multi-Account AWS IAM Access Advisor API
 """
 from __future__ import absolute_import
 
-import sys
-import os.path
+from pathlib import Path
 
 from setuptools import setup, find_packages
 
+from aardvark import __about__
 
-ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__)))
-
-# When executing the setup.py, we need to be able to import ourselves, this
-# means that we need to add the src/ directory to the sys.path.
-sys.path.insert(0, ROOT)
-
-about = {}
-with open(os.path.join(ROOT, "aardvark", "__about__.py")) as f:
-    exec(f.read(), about)
-
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 install_requires = [
     'requests',
@@ -48,21 +40,15 @@ tests_require = [
     'pexpect>=4.2.1'
 ]
 
-docs_require = [
-]
-
-dev_requires = [
-]
-
 
 setup(
-    name=about["__title__"],
-    version=about["__version__"],
-    author=about["__author__"],
-    author_email=about["__email__"],
-    url=about["__uri__"],
-    description=about["__summary__"],
-    long_description=open(os.path.join(ROOT, 'README.md')).read(),
+    name=__about__.__title__,
+    version=__about__.__version__,
+    author=__about__.__author__,
+    author_email=__about__.__email__,
+    url=__about__.__uri__,
+    description=__about__.__summary__,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
@@ -70,8 +56,6 @@ setup(
     install_requires=install_requires,
     extras_require={
         'tests': tests_require,
-        'docs': docs_require,
-        'dev': dev_requires,
     },
     entry_points={
         'console_scripts': [
